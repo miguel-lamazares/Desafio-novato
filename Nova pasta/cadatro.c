70fbef2e008e0a8814e708a7cac990edaa4c074c
Dexzn08 <miguellamazares8@gmail.com>
2025-04-30 12:02:32 -0300

    import

---
+++ Nova pasta/cadatro.c
@@ -0,0 +1,98 @@
+// Criado por Miguel Lamazares
+
+/* IMPORTANDO BIBLIOTECAS NECESSÁRIAS 
+1- stdio.h: Para funções de entrada e saída
+2- locale.h: Permitindo o uso de caracteres especiais
+*/
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    
+    // Definindo o locale para o idioma português do Brasil com a ISO 8859-1
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    // Declarando variáveis
+    char estado[20], card_id[4], city[20], estado1[20], card_id1[4], city1[20];
+    strcpy(card_id, "A01");
+    strcpy(card_id1, "B01");
+    int populacao, ponto_turistico, ponto_turistico1, populacao1;
+    float area, pib, area1, pib1;
+ 
+    // Exibindo mensagem de boas-vindas
+    printf("Bem-vindo ao sistema de cadastro de cidades!\n\n");
+
+    // Solicitando informações do usuário
+    printf("Digite o nome do estado: ");
+    scanf("%s", estado);
+    
+    printf("Digite o nome da cidade: ");
+    scanf("%s", city);
+    
+    printf("Digite o número de habitantes: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &ponto_turistico);
+
+    // Exibindo mensagem de confirmação
+    printf("\nDados coletados com sucesso!\n\nIniciando o cadastramento da segunda carta, pressione Enter para continuar\n");
+        getchar();
+        getchar();
+
+    // Solicitando informações para a segunda carta
+    printf("Digite o nome do estado: ");
+    scanf("%s", estado1);
+    
+    printf("Digite o nome da cidade: ");
+    scanf("%s", city1);
+
+    printf("Digite o número de habitantes: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &ponto_turistico1);
+
+    // Exibindo mensagem de confirmação
+    printf("\nDados coletados com sucesso!\n\nPressione Enter para visualizar as cartas registradas\n\n");
+        getchar();
+        getchar();
+
+    // Confecção da carta 1
+    printf("------------CARTA-1-------------\n");
+    printf("Código: %s\n", card_id);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", city);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos turísticos: %d\n", ponto_turistico);
+    printf("\nPressione Enter para visualizar a segunda carta\n\n");
+        getchar();
+
+    // Confecção da carta 2
+    printf("------------CARTA-2-------------\n");
+    printf("Código: %s\n", card_id1);
+    printf("Estado: %s\n", estado1);
+    printf("Cidade: %s\n", city1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turísticos: %d\n", ponto_turistico1);
+    printf("\nObrigado por utilizar o sistema de cadastro de cidades!\n\nPressione Enter para finalizar o programa\n\n");
+        getchar();
+   
+    return 0;
+}
